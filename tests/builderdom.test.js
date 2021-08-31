517d281cf8716d7a536be9646cd2de8d196168b6
ru51a4 <ru51a4@mail.ru>
2021-08-31 13:23:30 +0500

    jest

---
+++ tests/builderdom.test.js
@@ -0,0 +1,25 @@
+const BuilderDOM = require('../lib/helpers/builderdom');
+
+const builderdom = new BuilderDOM();
+
+test('builderdom simple', () => {
+    let dom = builderdom.html_to_dom(`
+    <html>
+    <span>test</span>
+    </html>
+    `);
+    expect(dom.length).toBe(2);
+    expect(dom[1].innerTEXT).toBe('test');
+});
+
+test('builderdom comments', () => {
+    let dom = builderdom.html_to_dom(`
+    <html>
+    <!-- comment -->
+    // comment
+    <span>test</span>
+    </html>
+    `);
+    expect(dom.length).toBe(2);
+    expect(dom[1].innerTEXT).toBe('test');
+});

916376ae14b078951a75b57b04e0e8e2e3907f7d
ru51a4 <ru51a4@mail.ru>
2021-09-02 13:05:14 +0500

    jest

--- tests/builderdom.test.js
+++ tests/builderdom.test.js
@@ -1,4 +1,7 @@
 const BuilderDOM = require('../lib/helpers/builderdom');
+const axios = require('axios').default;
+const documentServer = require('../lib/documentServer');
+
 
 const builderdom = new BuilderDOM();
 
@@ -22,4 +25,14 @@ test('builderdom comments', () => {
     `);
     expect(dom.length).toBe(2);
     expect(dom[1].innerTEXT).toBe('test');
+});
+
+test('builderdom real website', async () => {
+        expect.assertions(1);
+        response = await axios.get('https://education.github.com/pack')
+        const documentserver = new documentServer();
+        documentserver.build(response.data);
+        let breadcrumbSelected = documentserver.querySelector("nav[aria-label=Breadcrumb] ol > .breadcrumb-item-selected")[0];
+        let innerHTML = breadcrumbSelected.innerHTML();
+        expect(innerHTML).toBe(`<li class="breadcrumb-item-selected text-gray-light breadcrumb-item text-mono h5-mktg" aria-current="GitHub Student Developer Pack">GitHub Student Developer Pack</li>`);
 });

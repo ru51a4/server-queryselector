cdc65fdf18a1a89f6d05f790bd49b0d901af54b2
ru51a4 <ru51a4@mail.ru>
2021-09-16 08:06:03 +0500

    refactoring

--- tests/cssselectorparser.test.js
+++
@@ -1,9 +0,0 @@
-const cssSelectorParser = require('../lib/helpers/cssselectorparser');
-
-
-const cssselectorparser = new cssSelectorParser();
-
-test('cssselectorparser simple', () => {
-    let selectors = cssselectorparser.parse('nav[aria-label=Breadcrumb] ol > .breadcrumb-item-selected');
-    expect(selectors).toStrictEqual(JSON.parse(`[[{"key":"tag","value":"nav"},{"key":"aria-label","value":"Breadcrumb"}],[{"key":"tag","value":"ol"}],[{"key":"","value":">"}],[{"key":"class","value":"breadcrumb-item-selected"}]]`));
-});
